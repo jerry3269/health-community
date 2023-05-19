e483898b6db95ea0c5ebd8b9050fdbb394ccbc4d
지웅 양 <jerry3269@ajou.ac.kr>
2023-05-19 17:58:07 +0900

    first commit

---
+++ src/main/java/project/healthcommunity/global/advice/ControllerAdvice.java
@@ -0,0 +1,77 @@
+package project.healthcommunity.global.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import project.healthcommunity.global.error.ErrorCode;
+import project.healthcommunity.global.exception.*;
+
+import static project.healthcommunity.global.error.ErrorStaticField.*;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler(ArgumentNotValidException.class)
+    public ResponseEntity<ErrorCode> argumentNotValidExceptionHandler(ArgumentNotValidException ex) {
+        ErrorCode errorCode = ErrorCode.builder()
+                .errorCode(BAD_REQUEST)
+                .message(ex.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorCode);
+    }
+
+    @ExceptionHandler(BindingException.class)
+    public ResponseEntity<ErrorCode> bindingExceptionHandler(BindingException ex) {
+        ErrorCode errorCode = ErrorCode.builder()
+                .errorCode(BAD_REQUEST)
+                .message(ex.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorCode);
+    }
+
+    @ExceptionHandler(DuplicationLoginIdException.class)
+    public ResponseEntity<ErrorCode> duplicationLoginIdExceptionHandler(DuplicationLoginIdException ex) {
+        ErrorCode errorCode = ErrorCode.builder()
+                .errorCode(BAD_REQUEST)
+                .message(ex.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorCode);
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<ErrorCode> forbiddenExceptionHandler(ForbiddenException ex) {
+        ErrorCode errorCode = ErrorCode.builder()
+                .errorCode(FORBIDDEN)
+                .message(ex.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorCode);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorCode> notFoundExceptionHandler(NotFoundException ex) {
+        ErrorCode errorCode = ErrorCode.builder()
+                .errorCode(NOT_FOUND)
+                .message(ex.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorCode);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ErrorCode> unauthorizedExceptionHandler(UnauthorizedException ex) {
+        ErrorCode errorCode = ErrorCode.builder()
+                .errorCode(UNAUTHORIZED)
+                .message(ex.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorCode);
+    }
+
+    @ExceptionHandler(NotMatchException.class)
+    public ResponseEntity<ErrorCode> unauthorizedExceptionHandler(NotMatchException ex) {
+        ErrorCode errorCode = ErrorCode.builder()
+                .errorCode(NOT_MATCH)
+                .message(ex.getMessage())
+                .build();
+        return ResponseEntity.status(NOT_MATCH).body(errorCode);
+    }
+}
